778019247c4df138bb8c3c31399c1e5599079e4f
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2023-01-04 13:34:03 +0900

    Add files via upload

---
+++ baekjoon25083/baekjoon25083/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  baekjoon25083
+//
+//  Created by 이준협 on 2023/01/04.
+//
+
+import Foundation
+
+print(#"         ,r'"7"#)
+print(#"r`-_   ,'  ,/"#)
+print(#" \. ". L_r'"#)
+print(#"   `~\/"#)
+print(#"      |"#)
+print(#"      |"#)
+
